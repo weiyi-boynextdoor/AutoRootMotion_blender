0f916b24bea6e602063cb3c4e43658405b771ab1
weiyi-boynextdoor <55337314+weiyi-boynextdoor@users.noreply.github.com>
2025-06-07 20:26:10 +0800

    empty blend addon

---
+++ __init__.py
@@ -0,0 +1,17 @@
+
+bl_info = {
+    "name": "Autorootmotion",
+    "author": "weiyi",
+    "description": "Automatically generate root bone and motion for biped character",
+    "blender": (2, 80, 0),
+    "version": (0, 0, 1),
+    "location": "",
+    "warning": "",
+    "category": "Generic",
+}
+
+
+def register(): ...
+
+
+def unregister(): ...
