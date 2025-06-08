2efe6802e424f1a9bac6470e156876c3b44ccf53
weiyi-boynextdoor <55337314+weiyi-boynextdoor@users.noreply.github.com>
2025-06-08 19:14:55 +0800

    fix

--- __init__.py
+++ __init__.py
@@ -2,40 +2,40 @@ import bpy
 from . import auto_root_motion
 
 bl_info = {
-    "name": "Autorootmotion",
-    "author": "weiyi",
-    "description": "Automatically generate root bone and motion for biped character",
-    "blender": (2, 80, 0),
-    "version": (0, 0, 1),
-    "location": "",
-    "warning": "",
-    "category": "Generic",
+	"name": "Autorootmotion",
+	"author": "weiyi",
+	"description": "Automatically generate root bone and motion for biped character",
+	"blender": (2, 80, 0),
+	"version": (0, 0, 1),
+	"location": "",
+	"warning": "",
+	"category": "Generic",
 }
 
 class AutoRootMotionOperator(bpy.types.Operator):
-    bl_idname = "autorootmotion.run_operator"
-    bl_label = "Run Auto Root Motion"
-    bl_description = "Automatically add root bone and motion"
-    bl_options = {"REGISTER"}
+	bl_idname = "autorootmotion.run_operator"
+	bl_label = "Run Auto Root Motion"
+	bl_description = "Automatically add root bone and motion"
+	bl_options = {"REGISTER"}
 
-    def execute(self, context):
-        print("Test operator executed")
+	def execute(self, context):
+		return {"FINISHED"}
 
 class AutoRootMotionPanel(bpy.types.Panel):
-    bl_label = "Auto Root Motion"
-    bl_idname = "AutoRootMotion_PT_panel"
-    bl_space_type = "VIEW_3D"
-    bl_region_type = "UI"
-    bl_category = "AutoRootMotion"
+	bl_label = "Auto Root Motion"
+	bl_idname = "AUTOROOTMOTION_PT_panel"
+	bl_space_type = "VIEW_3D"
+	bl_region_type = "UI"
+	bl_category = "AutoRootMotion"
 
-    def draw(self, context):
-        layout = self.layout
-        layout.operator("autorootmotion.run_operator", text="Run Auto Root Motion")
+	def draw(self, context):
+		layout = self.layout
+		layout.operator("autorootmotion.run_operator", text="Run Auto Root Motion")
 
 def register():
-    bpy.utils.register_class(AutoRootMotionOperator)
-    bpy.utils.register_class(AutoRootMotionPanel)
+	bpy.utils.register_class(AutoRootMotionOperator)
+	bpy.utils.register_class(AutoRootMotionPanel)
 
 def unregister():
-    bpy.utils.unregister_class(AutoRootMotionOperator)
-    bpy.utils.unregister_class(AutoRootMotionPanel)
+	bpy.utils.unregister_class(AutoRootMotionOperator)
+	bpy.utils.unregister_class(AutoRootMotionPanel)
